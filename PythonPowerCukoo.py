092a10efffab8d3e1e74052b82b321a6ca376764
N3r3var1n3 <31942743+N3r3var1n3@users.noreply.github.com>
2017-09-13 23:41:52 -0500

    Some minor changes

--- PythonPowerCukoo.py
+++ PythonPowerCukoo.py
@@ -20,7 +20,7 @@ MaliciousFileREST = REST + 'tasks/create/file'
 MaliciousUrlREST = REST + 'tasks/create/url'
 MaliciousArchiveREST = REST +'task/create/submit'
 
-#
+'''
 IMAP_SERVER = input('IMAP Server: ')
 imap_port = input('Port: ')
 imap = imaplib.IMAP4_SSL(IMAP_SERVER,imap_port)
@@ -36,27 +36,27 @@ detach_dir = '.'
 if 'attachments' not in os.listdir(detach_dir):
     os.mkdir('attachments')
 
-userName = raw_input('Enter your GMail username:')
+emailaddress = input('Enter your GMail username:')
 passwd = getpass.getpass('Enter your password: ')
 
 try:
     imapSession = imaplib.IMAP4_SSL('imap.gmail.com')
-    typ, accountDetails = imapSession.login(userName, passwd)
+    typ, accountDetails = imapSession.login(emailaddress, passwd)
     if typ != 'OK':
-        print 'Not able to sign in!'
+        print('Not able to sign in!')
         raise
-    
+
     imapSession.select('[Gmail]/All Mail')
     typ, data = imapSession.search(None, 'ALL')
     if typ != 'OK':
-        print 'Error searching Inbox.'
+        print('Error searching Inbox.')
         raise
-    
+
     # Iterating over all emails
     for msgId in data[0].split():
         typ, messageParts = imapSession.fetch(msgId, '(RFC822)')
         if typ != 'OK':
-            print 'Error fetching mail.'
+            print('Error fetching mail.')
             raise
 
         emailBody = messageParts[0][1]
@@ -73,12 +73,11 @@ try:
             if bool(fileName):
                 filePath = os.path.join(detach_dir, 'attachments', fileName)
                 if not os.path.isfile(filePath) :
-                    print fileName
+                    print(fileName)
                     fp = open(filePath, 'wb')
                     fp.write(part.get_payload(decode=True))
                     fp.close()
     imapSession.close()
     imapSession.logout()
 except :
-    print 'Not able to download all attachments.'
-    '''
+    print('Not able to download all attachments.')
